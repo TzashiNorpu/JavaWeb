85a76744c6be68fc70024c821c02443daa1aed68
tzashinorpu <tzashinorpu@google.com>
2020-05-30 16:37:04 +0800

    tututu4

--- bookstore01/src/com/tz/web/BookServlet.java
+++ bookstore01/src/com/tz/web/BookServlet.java
@@ -13,19 +13,28 @@ import java.util.List;
 public class BookServlet extends BaseServlet {
     BookDao bookService = new BookDaoImpl();
 
+    protected void add(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+    protected void delete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+    protected void update(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
     protected void list(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
             IOException {
-//1 通过 BookService 查询全部图书
+        //1 通过 BookService 查询全部图书
         List<Book> books = bookService.queryBooks();
-        for (int i = 0; i < books.size(); i++) {
-            System.out.println("哈哈"+books.get(i));
-        }
-        for (Book queryBook : books) {
-            System.out.println("哈哈"+queryBook);
-        }
-//2 把全部图书保存到 Request 域中
+//        for (int i = 0; i < books.size(); i++) {
+//            System.out.println("哈哈"+books.get(i));
+//        }
+//        for (Book queryBook : books) {
+//            System.out.println("哈哈"+queryBook);
+//        }
+        //2 把全部图书保存到 Request 域中
         req.setAttribute("books", books);
-//3、请求转发到/pages/manager/book_manager.jsp 页面
+        //3、请求转发到/pages/manager/book_manager.jsp 页面
         req.getRequestDispatcher("/pages/manager/book_manager.jsp").forward(req, resp);
     }
 }
