d65697d2713d2f8295f6f6e2c841cc374111b2b6
TzashiNorpu <yzsnuaa@126.com>
2020-06-01 22:33:13 +0800

    tututu11

--- cookie_session/src/com/tz/servlet/CookieServlet.java
+++ cookie_session/src/com/tz/servlet/CookieServlet.java
@@ -32,7 +32,6 @@ public class CookieServlet extends BaseServlet {
         response.addCookie(cookie1);
         response.getWriter().write("cookie修改成功");
         // cookie value 不支持中文 空格 括号 ... 可以用 base64先进行编码
-
         Cookie[] cookies = request.getCookies();
         Cookie desCookie = null;
         for (Cookie cookie :
@@ -47,5 +46,38 @@ public class CookieServlet extends BaseServlet {
             response.addCookie(desCookie);
             response.getWriter().write("cookie修改成功");
         }
+    }
+
+    protected void defaultLife(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Cookie defualtValue = new Cookie("defaultValue", "defaultValue");
+        defualtValue.setMaxAge(-1);
+        response.addCookie(defualtValue);
+    }
+
+    protected void deleteNow(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Cookie[] cookies = request.getCookies();
+        Cookie desCookie = null;
+        for (Cookie cookie :
+                cookies) {
+            if (cookie.getName().equals("key2")) {
+                desCookie = cookie;
+                break;
+            }
+        }
+        desCookie.setMaxAge(0);
+        response.addCookie(desCookie);
+    }
+
+    protected void life3600(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Cookie cookie = new Cookie("life3600", "life3600");
+        cookie.setMaxAge(3600);
+        response.addCookie(cookie);
+    }
+
+    protected void testPath(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 只有在浏览器访问 http://ip:port/contentPath/abc/xxx 路径时才可以在浏览器上查看到这个 cookie，也只有满足这种路径的请求浏览器才会把这个 cookie 带上
+        Cookie cookie = new Cookie("path1", "path1");
+        cookie.setPath(request.getContextPath() + "/abc");
+        response.addCookie(cookie);
     }
 }
